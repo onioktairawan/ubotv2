fd44fd87de2783952c11dae85cf4e9ca594849bf
onioktairawan <122547358+onioktairawan@users.noreply.github.com>
2025-04-16 16:55:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from threading import Thread
-from scripts.bot import client
-from scripts.userbot import start, userbot
+import asyncio
+from scripts import userbot, bot
 
-def start_bot():
-    client.run_until_disconnected()
-
-def start_userbot():
-    userbot.start(phone_number)
-    userbot.run_until_disconnected()
+async def main():
+    await asyncio.gather(
+        userbot.start(),
+        bot.start()
+    )
 
 if __name__ == "__main__":
-    bot_thread = Thread(target=start_bot)
-    userbot_thread = Thread(target=start_userbot)
-
-    bot_thread.start()
-    userbot_thread.start()
-
-    bot_thread.join()
-    userbot_thread.join()
+    asyncio.run(main())
